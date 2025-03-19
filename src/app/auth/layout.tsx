9af45103d0af66f79066837f0b4791f39b453d27
Saksham Kamboj <saksham@ekvayu.com>
2025-03-19 16:17:48 +0530

    feat: Update favicon and add metadata to auth layout
    This commit updates the favicon and adds metadata to the authentication layout.
    
    Key changes:
    
    -   Updated the favicon.
    -   Added `generateMetadata` function to `auth/layout.tsx` to define the page title and description for SEO.

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,19 @@
 import React from "react";
+import { Metadata } from "next/types";
+
+// Static metadata
+// export const metadata: Metadata = {
+//   title: "Authentication | Phishing Admin",
+//   description: "Login to access the Phishing Admin Dashboard",
+// };
+
+// Alternatively, you can use generateMetadata for dynamic metadata
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Authentication | Phishing Admin",
+    description: "Login to access the Phishing Admin Dashboard",
+  };
+}
 
 export default function LoginLayout({
   children,
