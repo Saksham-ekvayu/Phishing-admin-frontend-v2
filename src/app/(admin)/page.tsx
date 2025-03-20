5743d3e194529110d275aadab2e20685c690bd8f
Saksham Kamboj <saksham@ekvayu.com>
2025-03-20 13:11:54 +0530

    update dashboard page

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,3 +1,12 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 export default function DashboardPage() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Box>
+      <Typography variant="h6">DashboardPage</Typography>
+      {/* Your Dashboard content */}
+    </Box>
+  );
 }
