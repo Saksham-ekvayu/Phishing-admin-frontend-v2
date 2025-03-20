c6556f730f2210bffea9ff6e3a4c0e4ef543b3e3
Saksham Kamboj <saksham@ekvayu.com>
2025-03-20 18:26:58 +0530

    update theme switcher

--- src/components/common/user-profile-header.tsx
+++ src/components/common/user-profile-header.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useColorScheme } from "@mui/material/styles";
 import {
   Avatar,
   Menu,
@@ -7,23 +8,53 @@ import {
   IconButton,
   Stack,
   ListItemIcon,
+  Tooltip,
+  Popover,
+  Box,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Divider,
 } from "@mui/material";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import Image from "next/image";
 import EditIcon from "@mui/icons-material/Edit";
 import LockIcon from "@mui/icons-material/Lock";
 import PaletteIcon from "@mui/icons-material/Palette";
 import LogoutIcon from "@mui/icons-material/Logout";
-import user_logo from "@/assets/IAF_logo.jpg";
-import { ThemeSwitcher } from "@toolpad/core/DashboardLayout";
+import SettingsIcon from "@mui/icons-material/Settings";
 
-export default function UserProfileHeader() {
+function CustomThemeSwitcher({ onClose }: { onClose: () => void }) {
+  const { mode, setMode } = useColorScheme();
+
+  const handleThemeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMode(event.target.value as "light" | "dark" | "system");
+    onClose();
+  };
+
+  return (
+    <Box sx={{ p: 2 }}>
+      <FormControl>
+        <FormLabel>Theme</FormLabel>
+        <RadioGroup value={mode} onChange={handleThemeChange}>
+          <FormControlLabel value="light" control={<Radio />} label="Light" />
+          <FormControlLabel value="system" control={<Radio />} label="System" />
+          <FormControlLabel value="dark" control={<Radio />} label="Dark" />
+        </RadioGroup>
+      </FormControl>
+    </Box>
+  );
+}
+
+export default function UserProfile() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
+  const [themeAnchorEl, setThemeAnchorEl] = useState<null | HTMLElement>(null);
 
-  const handleClick = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
+  const open = Boolean(anchorEl);
+  const themeOpen = Boolean(themeAnchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     setAnchorEl(event.currentTarget);
   };
 
@@ -31,14 +62,18 @@ export default function UserProfileHeader() {
     setAnchorEl(null);
   };
 
+  const handleThemeClick = (event: React.MouseEvent<HTMLElement>) => {
+    setThemeAnchorEl(event.currentTarget);
+  };
+
+  const handleThemeClose = () => {
+    setThemeAnchorEl(null);
+  };
+
   return (
     <Stack direction="row" alignItems="center" spacing={1}>
       <IconButton onClick={handleClick} sx={{ p: 0 }}>
-        <Image
-          alt="User logo"
-          src={user_logo}
-          className="rounded-full h-10 w-10"
-        />
+        <Avatar alt="User" src="/path-to-user-image.jpg" />
         <Typography variant="body1" sx={{ ml: 1 }}>
           John Doe
         </Typography>
@@ -64,13 +99,13 @@ export default function UserProfileHeader() {
           </ListItemIcon>
           Change Password
         </MenuItem>
-        <MenuItem onClick={handleClose}>
+        <MenuItem onClick={handleThemeClick}>
           <ListItemIcon>
             <PaletteIcon fontSize="small" />
           </ListItemIcon>
-          <Typography variant="inherit">Change Theme</Typography>
-          <ThemeSwitcher />
+          Change Theme
         </MenuItem>
+        <Divider />
         <MenuItem onClick={handleClose}>
           <ListItemIcon>
             <LogoutIcon fontSize="small" />
@@ -78,6 +113,15 @@ export default function UserProfileHeader() {
           Logout
         </MenuItem>
       </Menu>
+
+      <Popover
+        open={themeOpen}
+        anchorEl={themeAnchorEl}
+        onClose={handleThemeClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      >
+        <CustomThemeSwitcher onClose={handleThemeClose} />
+      </Popover>
     </Stack>
   );
 }
