63ede24934def71b081c919ac5e75425010689ae
Saksham Kamboj <saksham@ekvayu.com>
2025-03-20 10:03:34 +0530

    feat: Remove admin dashboard layout and replace with placeholder
    This commit removes the existing admin dashboard layout and replaces it with a simple placeholder. This change simplifies the layout and removes unnecessary components

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,12 +1,6 @@
 "use client";
 
 import React from "react";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/layout/app-sidebar";
 import { Separator } from "@/components/ui/separator";
 import {
   Breadcrumb,
@@ -26,35 +20,6 @@ export default function DashboardLayout({
 }) {
   const breadcrumbs = useBreadcrumbs();
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b border-gray-300">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                {breadcrumbs.map((item, index) => (
-                  <React.Fragment key={item.href}>
-                    <BreadcrumbItem>
-                      {item.isCurrentPage ? (
-                        <BreadcrumbPage>{item.label}</BreadcrumbPage>
-                      ) : (
-                        <BreadcrumbLink asChild>
-                          <Link href={item.href}>{item.label}</Link>
-                        </BreadcrumbLink>
-                      )}
-                    </BreadcrumbItem>
-                    {index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
-                  </React.Fragment>
-                ))}
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+    <div className="">Dashboard</div>
   );
 }
