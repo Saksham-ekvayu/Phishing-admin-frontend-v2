65f215c239fe9c6692714c212eea23a34c13d189
Saksham Kamboj <saksham@ekvayu.com>
2025-03-20 10:53:14 +0530

    feat: Remove admin dashboard layout components
    This commit removes the admin dashboard layout components, including the sidebar, header, and breadcrumbs, and simplifies the layout.
    
    Key changes:
    
    - Removed SidebarProvider, SidebarInset, and SidebarTrigger components.
    - Removed AppSidebar component.
    - Removed header with breadcrumbs.
    - Removed custom dark mode variant and theme variables for the sidebar in globals.css.
    - Simplified the DashboardLayout component.

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,60 +1,11 @@
 "use client";
 
 import React from "react";
-import { Separator } from "@/components/ui/separator";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { useBreadcrumbs } from "@/helpers/breadcrumb.helper";
-import Link from "next/link";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/layout/app-sidebar";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const breadcrumbs = useBreadcrumbs();
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset className="md:ml-[185px]">
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1 md:hidden" />
-            <Separator orientation="vertical" className="mr-2 h-4 md:hidden" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                {breadcrumbs.map((item, index) => (
-                  <React.Fragment key={item.href}>
-                    <BreadcrumbItem>
-                      {item.isCurrentPage ? (
-                        <BreadcrumbPage>{item.label}</BreadcrumbPage>
-                      ) : (
-                        <BreadcrumbLink asChild>
-                          <Link href={item.href}>{item.label}</Link>
-                        </BreadcrumbLink>
-                      )}
-                    </BreadcrumbItem>
-                    {index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
-                  </React.Fragment>
-                ))}
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <main>{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <div className="">{children}</div>;
 }
